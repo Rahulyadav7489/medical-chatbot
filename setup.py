1392525949b8cd4de914efa4ae5d76927193313e
Rahulyadav7489 <rahulyadav748969@gmail.com>
2025-12-14 21:19:32 +0530

    project setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rahul Yadav",
+    author_email="rahul.yadav@example.com",
+    packages=find_packages(),
+    install_requires=[],
+)
